b257b537f9c7105cfdc7f2333a4b561887d08f67
Damian Ezequiel Di Zeo <damiandizeo@gmail.com>
2020-11-23 00:29:55 -0300

    fixes

--- src/RNMoPubRewardedVideo.js
+++ src/RNMoPubRewardedVideo.js
@@ -7,7 +7,7 @@ const emitter = new NativeEventEmitter(RNMoPubRewardedVideo);
 module.exports = {
     initializeSdkForRewardedVideoAd: (adUnitId:string) => RNMoPubRewardedVideo.initialize(adUnitId),
     loadRewardedVideoAdWithAdUnitID: (adUnitId: string) => RNMoPubRewardedVideo.loadRewardedVideo(adUnitId),
-    presentRewardedVideoAdForAdUnitID: (adUnitId: string, currencyType:string, amount: number, promise:()=>{}) => RNMoPubRewardedVideo.userClickedToWatchAd(adUnitId, currencyType, amount, promise),
+    presentRewardedVideoAdForAdUnitID: (adUnitId: string) => RNMoPubRewardedVideo.userClickedToWatchAd(adUnitId),
     availableRewardsForAdUnitID: (adUnitId:string, promise:()=>{}) => RNMoPubRewardedVideo.availableRewardsForAdUnitID(adUnitId,promise),
     hasAdAvailableForAdUnitID:(adUnitId:string, promise:()=>{}) =>RNMoPubRewardedVideo.hasAdAvailableForAdUnitID(adUnitId,promise),
     addEventListener: (eventType: string, listener: Function)  => emitter.addListener(eventType, listener),
