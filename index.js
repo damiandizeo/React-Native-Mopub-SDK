eb21275de2b7e444b63ff4af4c7003c2ddcacafc
Damian Ezequiel Di Zeo <damiandizeo@gmail.com>
2020-11-28 00:01:45 -0300

    fixes

--- index.js
+++ index.js
@@ -1,12 +1,4 @@
 import RNMoPubWrapperSDK from './src/RNMoPubWrapperSDK';
-import RNMoPubInterstitial from './src/RNMoPubInterstitial';
-import MoPubBanner from './src/MoPubBanner';
-import RNMoPubRewardedVideo from './src/RNMoPubRewardedVideo';
-import RNNativeAdView from './src/RNNativeAdView'
 module.exports = {
-	RNMoPubWrapperSDK,
-    RNMoPubInterstitial,
-    MoPubBanner,
-    RNMoPubRewardedVideo,
-    RNNativeAdView
+	RNMoPubWrapperSDK
 };
