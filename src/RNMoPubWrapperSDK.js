855e2ca56d4e2c4a1486cfe44ebfd8abcf976b81
Damian Ezequiel Di Zeo <damiandizeo@gmail.com>
2020-11-27 23:35:32 -0300

    fix

--- src/RNMoPubWrapperSDK.js
+++ src/RNMoPubWrapperSDK.js
@@ -4,7 +4,7 @@ const { RNMoPubWrapperSDK } = NativeModules;
 const emitter = new NativeEventEmitter(RNMoPubWrapperSDK);
 
 module.exports = {
-    initialize: (interstitialAdUnitId: string, rewardedVideoAdUnitId: string) => RNMoPubRewardedVideo.initialize(interstitialAdUnitId, rewardedVideoAdUnitId),
+    initialize: (interstitialAdUnitId: string, rewardedVideoAdUnitId: string) => RNMoPubWrapperSDK.initialize(interstitialAdUnitId, rewardedVideoAdUnitId),
     loadRewardedVideo: (adUnitId: string) => RNMoPubWrapperSDK.loadRewardedVideo(adUnitId),
     presentRewardedVideo: (adUnitId: string) => RNMoPubWrapperSDK.presentRewardedVideo(adUnitId),
     loadInterstitial: (adUnitId: string) => RNMoPubWrapperSDK.loadInterstitial(adUnitId),
